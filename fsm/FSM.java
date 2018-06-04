f1f5153388c9c649d20943181dc59a686b87cc77
gzinck <graeme.zinck@gmail.com>
2018-06-04 14:34:00 -0300

    Added a few small changes.

--- fsm/FSM.java
+++ fsm/FSM.java
@@ -1,7 +1,7 @@
 package fsm;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.*;
 
 public abstract class FSM {
 	//--- Constant Values  -------------------------------------------------------------------------
@@ -13,9 +13,10 @@ public abstract class FSM {
 	
 	//--- Instance Variables  ----------------------------------------------------------------------
 	
+	protected ArrayList<State> states;
 	/** HashMap<String, ArrayList<Transition>> containing all the transitions from a given state with
 	 * various events that are possible*/
-	protected HashMap<String, ArrayList<Transition>> transitions;
+	protected HashMap<State, ArrayList<Transition>> transitions;
 	/** String object possessing the identification for this FSM object*/
 	protected String id;
 	
@@ -24,7 +25,7 @@ public abstract class FSM {
 	/**
 	 * Constructor for an FSM object that takes in a file encoding the contents of the FSM.
 	 * 
-	 * @param in - File readin order to create the FSM.
+	 * @param in - File read in order to create the FSM.
 	 */
 	public FSM(File in) {
 		id = "";
@@ -38,7 +39,32 @@ public abstract class FSM {
 		id = "";
 	}
 	
-	//--- Operations  ------------------------------------------------------------------------------
+	//--- Single-FSM Operations  ------------------------------------------------------------------------------
+	/**
+	 * Renames all the states in the set of states in the FSM so that
+	 * states are named sequentially ("0", "1", "2"...).
+	 */
+	public void renameStates() {
+		int index = 0;
+		for(State state : states)
+			state.setStateName(index + "");
+	} // renameStates()
+	
+	/**
+	 * Searches through the graph represented by the transitions hashmap, and removes
+	 * disjoint elements.
+	 * 
+	 * Algorithm starts from all initial States, and adds them to a queue. They are
+	 * then placed into the new transitions HashMap, and all States reachable by these
+	 * initial states are placed in a queue for processing. Once dealt with, a State is
+	 * added to a HashSet to keep track of what has already been seen.
+	 * 
+	 * @return - Returns an FSM object representing the accessible version of the current
+	 * FSM. 
+	 */
+	public abstract FSM makeAccessible();
+	
+	//--- Multi-FSM Operations  ------------------------------------------------------------------------------
 	
 	/**
 	 * Performs a union operation on two FSMs and returns the result.
@@ -47,9 +73,7 @@ public abstract class FSM {
 	 * FSM.
 	 * @return - The result of the union.
 	 */
-	public FSM union(FSM other) {
-		
-	} // union(FSM)
+	public abstract FSM union(FSM other);
 	
 	/**
 	 * Performs a product or intersection operation on two FSMs and returns the result.
